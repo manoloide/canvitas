088516bdce48998d0573821ba64286c62b01b947
manoloide <manoloide@gmail.com>
2014-08-15 19:14:09 -0300

    Agregue muchas funciones parecidas a processing

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@ window.onload = function() {
     init();
 }
 
-
 var coding;
 window.onkeydown = function(e) {
     if (e.ctrlKey) {
@@ -26,7 +25,6 @@ window.onkeydown = function(e) {
 
 function init() {
     coding = document.getElementById("coding");
-
     canvas = document.getElementById("canvis");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -34,7 +32,7 @@ function init() {
     width = canvas.width;
     height = canvas.height;
     spaceTab = "  ";
-    generar();
+    //generar();
     autoFormat();
 
     frameCount = -1;
@@ -84,91 +82,136 @@ function compilar(){
     document.body.appendChild(script);
 }
 
-
-function generar() {
-    var c = document.getElementById("canvis");
-    var ctx = c.getContext("2d");
-    var width = canvas.width;
-    var height = canvas.height;
-
-    var grd = ctx.createLinearGradient(0, 0, 0, height);
-    grd.addColorStop(0, "#ADEDDA");
-    grd.addColorStop(1, "#63DA9E");
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, width, height);
-    ctx.fill();
-
-    circulin(ctx, width / 2, height / 2, height * 0.8, 200);
-
-    var cant = Math.random() * 3 + 2;
-    var tam = 26;
-    for (var i = 0; i < cant; i++) {
-        var x = (0.1 + Math.random() * 0.8) * width;
-        var y = (0.1 + Math.random() * 0.8) * height;
-        ctx.beginPath();
-        ctx.lineWidth = 0.1;
-        ctx.shadowBlur = 2;
-        ctx.shadowOffsetX = 1;
-        ctx.shadowColor = "#00A668";
-        ctx.strokeStyle = "#FEFEFE";
-        for (var j = y; j < height; j += 6) {
-            ctx.moveTo(x, j);
-            ctx.lineTo(x, j + 3);
-            ctx.stroke();
-        }
-        ctx.shadowOffsetX = 0;
-
-        ctx.fillStyle = "#3A3A34";
-        ellipse(ctx, x, y, tam, tam);
-        ctx.stroke();
-        ctx.fill();
-        ctx.shadowBlur = 0;
-        ctx.beginPath();
-        ctx.fillStyle = "B0FA29";
-        ctx.strokeStyle = "#33332F";
-        ellipse(ctx, x, y, tam * 0.2, tam * 0.2);
-        ctx.stroke();
-        ctx.fill();
-        ctx.beginPath();
-        ctx.lineWidth = 3;
-        ctx.arc(x, y, tam * 1.2, Math.random() * (Math.PI / 4) * 3 + (Math.PI / 4) * 3, Math.PI * 1.5 + Math.random() * (Math.PI / 4) * 3);
-        ctx.strokeStyle = "#B0FA29";
-        ctx.stroke();
-
-
-    }
-}
-
-function ellipse(context, cx, cy, rx, ry) {
-    context.save(); // save state
-    context.beginPath();
-    context.translate(cx - rx, cy - ry);
-    context.scale(rx, ry);
-    context.arc(1, 1, 1, 0, 2 * Math.PI, false);
-    context.restore(); // restore to original state
-}
-
-function circulin(ctx, x, y, tam, cant) {
-    ctx.lineWidth = 2;
-    var da = (Math.PI * 2) / cant;
-    var rad = tam / 2;
-    ctx.strokeStyle = "#FEFEFE";
-    for (var i = 0; i < cant; i++) {
-        ctx.beginPath();
-        ctx.lineCap = "butt";
-        var dx = Math.cos(da * i);
-        var dy = Math.sin(da * i);
-        var tt = 0.6 + (Math.random() * 0.4);
-        var ct = rad * 0.01;
-        ctx.moveTo(x + dx * rad, y + dy * rad);
-        ctx.lineTo(x + dx * rad * tt, y + dy * rad * tt);
-        ctx.stroke();
-        ellipse(ctx, x + dx * (rad * tt - ct), y + dy * (rad * tt - ct), ct, ct);
-        ctx.stroke();
-    }
-    ellipse(ctx, x, y, rad, rad);
-    ctx.stroke();
-}
-
 var loop = function(){
+
+}
+
+//// "Processing"
+// Colors
+function background(c){
+  ctx.fillStyle = c;
+  ctx.fillRect(0, 0, width, height);
+  ctx.fill();
+}
+
+function color(r,g,b){
+    if(r < 0) r = 0;
+    if(r > 256) r = 255;
+    if(g < 0) g = 0;
+    if(g > 256) g = 255;
+    if(b < 0) b = 0;
+    if(b > 256) b = 255;
+    if (g === undefined) {
+          g = b = r;
+    }
+    var col = "#";
+    var cc = Math.floor(r).toString(16);
+    if(cc.length == 1) cc = "0"+cc;
+    col += cc;
+    cc = Math.floor(g).toString(16);
+    if(cc.length == 1) cc = "0"+cc;
+    col += cc;
+    cc = Math.floor(b).toString(16);
+    if(cc.length == 1) cc = "0"+cc;
+    col += cc;
+    return col;
+}
+function fill(c){
+    ctx.fillStyle = c;
+}
+function stroke(c){
+    ctx.strokeStyle = c;
+}
+// Draw
+function arc(x, y, d, a, a2){
+    ctx.beginPath();
+    ctx.arc(x,y,d/2,a,a2);
+}
+function circle(x, y, d){
+    ctx.beginPath();
+    ctx.arc(x,y,d/2,0,2*Math.PI);
+}
+function line(x, y, x2, y2){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+    ctx.lineTo(x2,y2);
+}
+function rect(x, y, w, h){
+    ctx.fillRect(x, y, w, h);
+}
+function star(x, y, d, c, a, md){
+    var r = d/2;
+    var da = Math.PI*2/(c*2);
+    ctx.beginPath();
+    ctx.moveTo(x+Math.cos(a)*r, y+Math.sin(a)*r);
+    for(var i = 1; i < c*2; i+=2){
+        ctx.lineTo(x+Math.cos(a+da*i)*r*md, y+Math.sin(a+da*i)*r*md);
+        ctx.lineTo(x+Math.cos(a+da*(i+1))*r, y+Math.sin(a+da*(i+1))*r);   
+    }
+    ctx.closePath();
+}
+function poly(x, y, d, c, a){
+    var r = d/2;
+    var da = Math.PI*2/c;
+    ctx.beginPath();
+    ctx.moveTo(x+Math.cos(a)*r, y+Math.sin(a)*r);
+    for(var i = 1; i < c; i++){
+        ctx.lineTo(x+Math.cos(a+da*i)*r, y+Math.sin(a+da*i)*r);   
+    }
+    ctx.closePath();
+}
+
+//Math
+var PI = Math.PI;
+var TWO_PI = Math.PI*2;
+function abs(v){
+    return Math.abs(v);
+}
+function atan2(y, x){
+    return Math.atan2(y, v);
+}
+function ceil(v){
+    return Math.ceil(v);
+}
+function cos(v){
+    return Math.cos(v);
+}
+function exp(v){
+    return Math.exp(v);
+}
+function floor(v){
+    return Math.floor(v);
+}
+function log(v){
+    return Math.log(v);
+}
+function max(v, v2){
+    return Math.max(v, v2);
+}
+function min(v, v2){
+    return Math.min(v, v2);
+}
+function pow(v, v2){
+    return Math.pow(v, v2);
+}
+function random(min, max){
+    if (min === undefined) {
+        return Math.random();
+    }else if(max === undefined){
+        return Math.random()*min;
+    }else{
+        return min + (max-min) * Math.random(); 
+    }
+}
+function round(v){
+    return Math.round(v);
+}
+function sin(v){
+    return Math.sin(v);
+}
+function sqrt(v){
+    return Math.sqrt(v);
+}
+function tan(v){
+    return Math.tan(v);
 }
